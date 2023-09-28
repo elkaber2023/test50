6868e49d6f4583e1891726c2dc8377f00a8f659b
elkaber2023 <pop20222021@gmail.com>
2023-09-28 22:53:58 +0300

    new update from branch

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
+// ! My Js File | testing One
+
 let div = document.createElement("div");
+div.style.backgroundColor = "green";
 
 let p = document.createElement("p");
-p.style.cssText = "color:greenyallow;background-color:black;";
+p.style.cssText = "color:greenyallow;background-color:red;";
+
+let span = document.createElement("span");
